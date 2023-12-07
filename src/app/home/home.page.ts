a03674d8e75e8e9b39b0cedcd5441f16c0a572fb
ISS Builder <iss@macbuilder01.int-link.com>
2023-12-07 11:46:30 +0000

    updated with example to show issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  line1?: IonItemSliding 
+
+  option2enabled: boolean = false
+
   constructor() {}
+
+  doSomething() {
+    console.log("doing something...")
+    this.line1?.close()
+  }
+
+  doNothing() {
+    console.log("doing nothing...")
+  }
+
+  dragEvent(event: any, slider: IonItemSliding) {
+    this.line1 = slider
+    console.log(JSON.stringify(event), slider)
+  }
+
+  addOption() {
+    this.option2enabled = true
+    this.line1?.close()
+    this.line1?.open(undefined)
+  }
 
 }
